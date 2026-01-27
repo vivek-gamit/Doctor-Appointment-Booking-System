01ecde997ba3f0382295e3457e1c787c17eec982
vivek-gamit <vgamit637@gmail.com>
2026-01-27 12:41:49 +0530

    LoginPage Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
-
+import React from 'react'
 import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/About'
-import Login from './pages/Login'
-import MyProfile from './pages/MyProfile'
-import MyAppointments from './pages/MyAppointments'
-import Appointments from './pages/Appointment'
-import Footer from './components/Footer'
 import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Appointment from './pages/Appointment'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Footer from './components/Footer'
 
-
-
-
-function App() {
+const App = () => {
+  
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
       <Navbar />
-      
-
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/my-profile' element={<MyProfile />} />
-        <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/appointment/:docId' element={<Appointments />} />
         <Route path='/doctors' element={<Doctors />} />
         <Route path='/doctors/:speciality' element={<Doctors />} />
-       
-      
-
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
       </Routes>
-
-      <Footer/>
+      <Footer />
     </div>
   )
 }
